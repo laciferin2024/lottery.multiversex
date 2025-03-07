ed7c0ed5c75e3d3522c7248f1f80251cac5ee2ff
Nasfame <laciferin2024@protonmail.com>
2025-03-07 06:51:16 +0530

    lottery: test fixes

--- tests/lottery_scenario_rs_test.rs
+++ tests/lottery_scenario_rs_test.rs
@@ -4,11 +4,11 @@ fn world() -> ScenarioWorld {
     let mut blockchain = ScenarioWorld::new();
 
     // blockchain.set_current_dir_from_workspace("relative path to your workspace, if applicable");
-    blockchain.register_contract("mxsc:output/lottery.multiversex.mxsc.json", lottery::ContractBuilder);
+    blockchain.register_contract("mxsc:output/lottery.mxsc.json", lottery::ContractBuilder);
     blockchain
 }
 
 #[test]
 fn empty_rs() {
-    world().run("scenarios/lottery.multiversex.scen.json");
+    world().run("scenarios/lottery.scen.json");
 }
